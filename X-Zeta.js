6626098b4c177366b822dd734ce1ec33972650da
3lectr0N!nj@ <aaronjs099@gmail.com>
2026-07-19 15:03:41 +0300

    Update X-Zeta.js
    Added Info of Project
    Added feature to change WireFrame Count

--- X-Zeta.js
+++ X-Zeta.js
@@ -1,5 +1,10 @@
 const XZ = window.XZ = {
     patched :false ,
+    info : {
+        developer:"3lectr0N!nj@",
+        name: "X-Zeta",
+        version: "1.0"
+    },
     loggers:{
         wireframe:false
     },
@@ -7,7 +12,7 @@ const XZ = window.XZ = {
         enabled:false,
         skipUI: true,
         count: {
-            min: 6,
+            min: 7,
             max: Infinity
         }
     },
@@ -33,9 +38,14 @@ const XZ = window.XZ = {
         const isUI = this.wireframe.skipUI && program && program.isUIProgram === true;
         if (!isUI && count >= this.wireframe.count.min && count <= this.wireframe.count.max) {args[0] = gl.LINES;}
     },
+    SetWireFrameCount(min=7,max=Infinity){
+            if (typeof max !== "number" || max <= 0 || typeof min !== "number" || min <= 0) return alert("Invalid count")
+            XZ.wireframe.count={max,min}
+        },
     PatchAll(){
         if(this.patched) return;
         this.patched = true;
+        //WIREFRAME PATCH
         [window.WebGLRenderingContext,window.WebGL2RenderingContext].forEach(Context => {
     if (!Context) return;
 
@@ -43,23 +53,26 @@ const XZ = window.XZ = {
     if (proto.drawElements)proto.drawElements = new Proxy(proto.drawElements, XZ.wireframeHandler);
     if (proto.drawElementsInstanced)proto.drawElementsInstanced = new Proxy(proto.drawElementsInstanced, XZ.wireframeHandler);
 });
-performance.now = function(){
+        //SPEED-HACk PATCH
+        performance.now = function(){
     if(XZ.speedhack.value === 1){return XZ.speedhack.originalPerformanceNow()};
     const realTime = XZ.speedhack.originalPerformanceNow();
     return XZ.speedhack.startFake + (realTime - XZ.speedhack.startReal)*XZ.speedhack.value};
     },
-    Hacks:{
+    Features:{
+        //WIREFRAME
         WireFrame(){
             XZ.wireframe.enabled = !XZ.wireframe.enabled
-            return alert("WireFrame: " + (XZ.wireframe.enabled ? "Activated" : "Deactivated"))
+            return console.log("WireFrame: " + (XZ.wireframe.enabled ? "Activated" : "Deactivated"))
         },
+        //SPEEDHACK
         SetSpeed(value){
             if (typeof value !== "number" || value <= 0) return alert("Invalid speed");
             const current = XZ.speedhack.originalPerformanceNow();
             XZ.speedhack.startFake = current;
             XZ.speedhack.startReal = current;
             XZ.speedhack.value = value;
-            alert("Speed Set to:-" + value)
+            return console.log("Speed Set to:-" + value)
    },
     }
 }
