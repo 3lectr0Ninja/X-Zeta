d72131495293e3a40db957b8cae4cf4a5e14c103
3lectr0N!nj@ <aaronjs099@gmail.com>
2026-07-26 23:33:01 +0300

    Update SpeedHack.js

--- SpeedHack.js
+++ SpeedHack.js
@@ -7,20 +7,20 @@ window.SpeedHack:{
         },
   patch(){
     performance.now = function() {
-            if (XZ.settings.speedhack.value === 1) {
-                return XZ.settings.speedhack.originalPerformanceNow()
+            if (SpeedHack.settings.value === 1) {
+                return SpeedHack.settings.originalPerformanceNow()
             };
-            const realTime = XZ.settings.speedhack.originalPerformanceNow();
-            return XZ.settings.speedhack.startFake + (realTime - XZ.settings.speedhack.startReal) * XZ.settings.speedhack.value
+            const realTime = SpeedHack.settings.originalPerformanceNow();
+            return SpeedHack.settings.startFake + (realTime - SpeedHack.settings.startReal) * SpeedHack.settings.value
         };
   },
     features:{
             SetSpeed(value) {
                 if (typeof value !== "number" || value <= 0) return alert("Invalid speed");
-                const current = XZ.settings.speedhack.originalPerformanceNow();
-                XZ.settings.speedhack.startFake = current;
-                XZ.settings.speedhack.startReal = current;
-                XZ.settings.speedhack.value = value;
+                const current = SpeedHack.settings.originalPerformanceNow();
+                SpeedHack.settings.startFake = current;
+                SpeedHack.settings.startReal = current;
+                SpeedHack.settings.value = value;
                 return console.log("Speed Set to:-" + value)
             },
         }
