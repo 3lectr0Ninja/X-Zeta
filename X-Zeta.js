b433c0cf76c6067d0c6a66c4ef4fb797594f682a
3lectr0N!nj@ <aaronjs099@gmail.com>
2026-07-26 23:56:24 +0300

    Update X-Zeta.js

--- X-Zeta.js
+++ X-Zeta.js
@@ -9,91 +9,40 @@ const XZ = window.XZ = {
     loggers: {
         wireframe: false
     },
-    settings: {
-        wireframe: {
-            enabled: false,
-            skipUI: true,
-            count: {
-                min: 7,
-                max: Infinity
-            },
-            onDraw(gl, args) {
-                const program = gl.getParameter(gl.CURRENT_PROGRAM);
-                const [mode, count, type, offset] = args;
-                if (XZ.loggers.wireframe) console.log({
-                    mode,
-                    count,
-                    type,
-                    offset,
-                    program
-                });
-                if (!this.enabled) return;
-                const isUI = this.skipUI && program && program.isUIProgram === true;
-                if (!isUI && count >= this.count.min && count <= this.count.max) {
-                    args[0] = gl.LINES;
-                }
-            },
-            wireframeHandler: {
-                apply(target, gl, args) {
-                    XZ.settings.wireframe.onDraw(gl, args);
-                    return Reflect.apply(target, gl, args);
-                }
-            },
-        },
-        speedhack: {
-            value: 1,
-            startReal: performance.now(),
-            startFake: performance.now(),
-            originalPerformanceNow: performance.now.bind(performance),
-        },
+    patches: [],
+    Pluginsurls:["https://raw.githubusercontent.com/3lectr0Ninja/X-Zeta/refs/heads/main/WireFrame.js","https://raw.githubusercontent.com/3lectr0Ninja/X-Zeta/refs/heads/main/SpeedHack.js"],
+    async DownloadPlugins(){
+    for (const url of this.Pluginsurls) {await this.LoadPlugin(url);XZ.PatchAll();}
     },
-    memory: [],
+    async LoadPlugin(url){
+        async function loadScript(url) {
+            try {
+      const res = await fetch(url);
+      const code = await res.text();
+      (0, eval)(code);
+    } catch (err) {console.error("Failed to load script:", url, err);
+                  }
+  }
+        await loadScript(url)
+        let spliturl = url.split("/")
+        let plugin = spliturl[spliturl.length-1].split(".")[0]
+        console.log(plugin," Loaded")
+        this.SetPlugin(window[plugin])
+    },
+    SetPlugin(plugin){
+        this.settings[plugin.name] = plugin.settings;
+        this.Features[plugin.name.toUpperCase()] = plugin.features;
+        this.patches.push(plugin.patch);
+        console.log(plugin.name," Set")
+    },
+    settings: {},
     scanned: 0,
     Scans: {},
     PatchAll() {
         if (this.patched) return;
+        this.patches.forEach((func)=>{func()})
         this.patched = true;
-        //WIREFRAME PATCH
-        [window.WebGLRenderingContext, window.WebGL2RenderingContext].forEach(Context => {
-            if (!Context) return;
-
-            const proto = Context.prototype;
-            if (proto.drawElements) proto.drawElements = new Proxy(proto.drawElements, XZ.settings.wireframe.wireframeHandler);
-            if (proto.drawElementsInstanced) proto.drawElementsInstanced = new Proxy(proto.drawElementsInstanced, XZ.settings.wireframe.wireframeHandler);
-        });
-        //SPEED-HACk PATCH
-        performance.now = function() {
-            if (XZ.settings.speedhack.value === 1) {
-                return XZ.settings.speedhack.originalPerformanceNow()
-            };
-            const realTime = XZ.settings.speedhack.originalPerformanceNow();
-            return XZ.settings.speedhack.startFake + (realTime - XZ.settings.speedhack.startReal) * XZ.settings.speedhack.value
-        };
     },
-    Features: {
-        WIREFRAME: {
-            SetWireFrameCount(min = 7, max = Infinity) {
-                if (typeof max !== "number" || max <= 0 || typeof min !== "number" || min <= 0) return alert("Invalid count")
-                XZ.settings.wireframe.count = {
-                    max,
-                    min
-                }
-            },
-            WireFrame() {
-                XZ.settings.wireframe.enabled = !XZ.settings.wireframe.enabled
-                return console.log("WireFrame: " + (XZ.settings.wireframe.enabled ? "Activated" : "Deactivated"))
-            },
-        },
-        SPEEDHACK: {
-            SetSpeed(value) {
-                if (typeof value !== "number" || value <= 0) return alert("Invalid speed");
-                const current = XZ.settings.speedhack.originalPerformanceNow();
-                XZ.settings.speedhack.startFake = current;
-                XZ.settings.speedhack.startReal = current;
-                XZ.settings.speedhack.value = value;
-                return console.log("Speed Set to:-" + value)
-            },
-        }
-    }
+    Features: {}
 };
-XZ.PatchAll();
+await XZ.DownloadPlugins()
